653b7c7f4c1b28bfea3158fd3595d11c4720ce27
Larry N <larryn35@gmail.com>
2021-04-03 23:34:48 -0700

    Initial Commit

---
+++ TextRecognitionDemo/TextRecognitionDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TextRecognitionDemoApp.swift
+//  TextRecognitionDemo
+//
+//  Created by Larry N on 4/3/21.
+//
+
+import SwiftUI
+
+@main
+struct TextRecognitionDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
