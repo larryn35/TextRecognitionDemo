7af23a197cf417af7c08749ae4bed427cd2737d1
Larry N <larryn35@gmail.com>
2021-04-05 14:27:56 -0700

    add sample drug bank

---
+++ TextRecognitionDemo/Helpers/Data.swift
@@ -0,0 +1,14 @@
+//
+//  Data.swift
+//  TextRecognitionDemo
+//
+//  Created by Larry N on 4/5/21.
+//
+
+import Foundation
+
+struct Data {
+  static let sampleDrugBank = [
+    "Losartan", "Lisinopril", "Simvastatin", "Metformin", "Atorvastatin", "Levothyroxine", "Amoxicillin", "Fluoxetine", "Omeprazole", "Escitalopram", "Citalopram", "Ibuprofen", "Hydrochlorothiazide"
+  ]
+}
